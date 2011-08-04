4d358047bc5689b27dd36942e3a7182f98e619da
kidfolk <xiaowu40@gmail.com>
2011-08-04 18:49:14 +0800

    添加匿名访问判断

--- src/org/kidfolk/tinyftp/PASSCmd.java
+++ src/org/kidfolk/tinyftp/PASSCmd.java
@@ -1,37 +1,36 @@
 package org.kidfolk.tinyftp;
 
 import android.util.Log;
+
 /**
  * Authentication password.
+ * 
  * @author kidfolk
- *
+ * 
  */
 public class PASSCmd extends FTPCmd {
 	private static final String TAG = "PASSCmd";
 	private String inputStr;
-	
-	
 
-	public PASSCmd(Session session,String inputStr) {
+	public PASSCmd(Session session, String inputStr) {
 		super(session);
 		this.inputStr = inputStr;
 	}
-
-
 
 	@Override
 	public void run() {
 		Log.v(TAG, "PASSCmd executing!");
 		String password = FTPCmd.getParameter(inputStr);
-		if(password.equals(GlobleConfig.password)){
+		if (GlobleConfig.allowAnonymous
+				|| password.equals(GlobleConfig.password)) {
+			// 允许匿名访问
 			session.sendReplyString("230 Logged on\r\n");
 			Log.v(TAG, "230 Logged on");
-		}else{
+		} else {
 			session.sendReplyString("530 Login or password incorrect!\r\n");
 			Log.v(TAG, "530 Login or password incorrect!");
 		}
 		Log.v(TAG, "PASSCmd complete!");
 	}
- 
+
 }
- 
