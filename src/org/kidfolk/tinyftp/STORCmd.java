f0d58c0e3b757bf1e723a64af87aafe14ff26eff
kidfolk <xiaowu40@gmail.com>
2011-08-04 18:51:28 +0800

    完成文件上传

--- src/org/kidfolk/tinyftp/STORCmd.java
+++ src/org/kidfolk/tinyftp/STORCmd.java
@@ -2,19 +2,20 @@ package org.kidfolk.tinyftp;
 
 import android.util.Log;
 
-public class STORCmd extends FTPCmd {
+public class STORCmd extends AbstractSTORCmd {
+	
 	private static final String TAG = "STORCmd";
 	private String inputStr;
 
-	public STORCmd(Session session,String inputStr) {
-		super(session);
+	public STORCmd(Session session, String inputStr) {
+		super(session, inputStr);
 		this.inputStr = inputStr;
 	}
 
 	@Override
 	public void run() {
-		Log.v(TAG, "STORCmd executind!");
-		//TODO
+		Log.v(TAG, "STORCmd executing!");
+		doStorOrAppe(getParameter(inputStr), false);
 	}
 
 }
