919b1b18197ee2dd6abb2abe1c30ee862248f509
kidfolk <xiaowu40@gmail.com>
2011-08-04 18:46:02 +0800

    添加一个变量用户控制匿名访问

--- src/org/kidfolk/tinyftp/GlobleConfig.java
+++ src/org/kidfolk/tinyftp/GlobleConfig.java
@@ -5,5 +5,6 @@ public class GlobleConfig {
 	protected static String username = "test";
 	protected static String password = "test";
 	protected static String rootdir = "/mnt/sdcard";
+	protected static boolean allowAnonymous = true;//允许匿名访问
 
 }
