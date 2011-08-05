458beb3b04e4ba53e6de7088b82285b92c51b639
kidfolk <xiaowu40@gmail.com>
2011-08-05 18:06:57 +0800

    添加变量，用于控制连接超时

--- src/org/kidfolk/tinyftp/GlobleConfig.java
+++ src/org/kidfolk/tinyftp/GlobleConfig.java
@@ -6,5 +6,6 @@ public class GlobleConfig {
 	protected static String password = "test";
 	protected static String rootdir = "/mnt/sdcard";
 	protected static boolean allowAnonymous = true;//允许匿名访问
+	public static final int SO_TIMEOUT_MS = 30000;
 
 }
